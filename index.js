548e0b0afc41b8e68352e35ae24ddb07e19f0203
Chris Flora <chris@dynamicviz.com>
2017-04-21 12:56:15 -0700

    concatenation in js is different than php

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ exports.handler = (event, context) => {
 
             context.succeed(
               generateResponse(
-                buildSpeechletResponse("The current score is ".scoreString, false),
+                buildSpeechletResponse("The current score is "+scoreString, false),
                 event.session.attributes
               )
             );
